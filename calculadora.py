721924b8a1e6458fd6081b41b624bb029932e648
Christiano Effgen Moises <christianoe.moises@gmail.com>
2025-08-31 21:23:27 -0300

    adicionando comentarios ao código fixed #2

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
-
+# Função calculadoura
 def calculadora():
+    # inicia um loop com opçoes para o usuario digitar
     while True:
         print("\n=== CALCULADORA SIMPLES ===")
         print("1 - Somar")
@@ -8,19 +9,26 @@ def calculadora():
         print("4 - Dividir")
         print("5 - Sair")
         
+        # variavel que armazena o que o usuario digitou
         opcao = input("Escolha uma opção: ")
 
+        # se o que o usuario digitou for o numero 5 o código para
         if opcao == "5":
             print("Encerrando a calculadora...")
             break
         
         try:
+            # esse variavel pede para o usuario digitar armazena o primeiro numero digitado
             num1 = float(input("Digite o primeiro número: "))
+            # esse variavel pede para o usuario digitar armazena o segundo numero digitado
             num2 = float(input("Digite o segundo número: "))
+
+            # esse bloco se o usuario digitar uma opção que nao conta na lista informa opção invalida e retora ao menu novamente
         except ValueError:
             print("⚠️ Entrada inválida, digite apenas números!")
             continue
 
+        # esse bloco de if e else seleciona a operação a ser feita
         if opcao == "1":
             print(f"Resultado: {num1} + {num2} = {num1 + num2}")
         elif opcao == "2":
